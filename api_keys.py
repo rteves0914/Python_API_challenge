0b3d3b02d6a1e0ea2265df2afa6364f54bde8141
Randall Teves <rteves0914@gmail.com>
2020-11-08 22:37:59 -0800

    Creating starter codes

---
+++ api_keys.py
@@ -0,0 +1,14 @@
+# New York Times api key
+api_ny = "h9nSpuUZ0fDKER4UsgoFTv1Emr2nAy9p"
+
+# Open Movie DB api key
+api_movie = "c1ddce1a"
+
+# Weather map api key
+api_weather = "cc504bff0b3241833baa8f7fe8b72972"
+
+# Google maps api key
+api_google = "AIzaSyA1IVFylDAURpO5DG3zYSOsW-P74nANBZQ"
+
+# Census api key
+apu_census = "8282d0e94fe799fc09fb9a7f817d31eb408fa38f"
